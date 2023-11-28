fff6e4f8235c5c632dd8bde1d6ca9b1e3dd34c2e
bramkamies <bramkamies@passionatepeople.io>
2023-11-28 15:17:24 +0100

    Change matrix card to use `plus-lighter` instead of `color-burn` as it had some visual glitches
    - Added card glare layer to matrix card

--- src/components/MatrixCard.vue
+++ src/components/MatrixCard.vue
@@ -1,55 +1,63 @@
 <script setup>
 import CardBase from "./CardBase.vue";
-import {onMounted, ref} from "vue";
+import {computed, onMounted, reactive, ref} from "vue";
+import {useIntervalFn} from "@vueuse/core";
 
 const numCols = 10;
 const numRows = 12;
 const alphabet = false
     ? ['ᚠ', 'ᚢ', 'ᚦ', 'ᚨ', 'ᚱ', 'ᚲ', 'ᚷ', 'ᚹ', 'ᚺ', 'ᚾ', 'ᛁ', 'ᛃ', 'ᛇ', 'ᛈ', 'ᛉ', 'ᛊ', 'ᛏ', 'ᛒ', 'ᛖ', 'ᛗ', 'ᛚ', 'ᛜ', 'ᛞ', 'ᛟ']
     : ['0', '1'];
-const randomRune = () => alphabet[Math.floor(Math.random() * alphabet.length)];
+const runeText = () => alphabet[Math.floor(Math.random() * alphabet.length)];
+const arr = (length, fn) => Array.from({length}, (_, i) => fn(i));
 
-const $el = ref(null);
-onMounted(() => {
-  setupRunes($el.value);
-});
+/** @type {{text:string, fresh:boolean}[][]} */
+const grid = reactive(
+    arr(numCols, x =>
+        arr(numRows, y =>
+            ({
+              text: runeText(),
+              fresh: Math.random() > 0.5,
+            })
+        )
+    )
+);
 
-/** @param {HTMLElement} container */
-function setupRunes(container) {
-  for (let x = 0; x < numCols; x++) {
-    const column = document.createElement('span');
-    column.classList.add('rune-col');
-    for (let y = 0; y < numRows; y++) {
-      const cell = document.createElement('span');
-      cell.innerText = randomRune();
-      cell.classList.add('rune');
-      if (Math.random() > 0.5) {
-        cell.classList.add('fresh');
-      }
-      column.appendChild(cell);
+useIntervalFn(() => {
+  const total = numCols * numRows;
+  // Try to turn some back on
+  change(.5 * total, cell => {
+    if (!cell.fresh) {
+      cell.fresh = true;
+      cell.text = runeText();
     }
-    container.appendChild(column);
+  });
+  // Disable half the cells
+  change(total * 0.5, cell => cell.fresh = false);
+
+  function change(amount, fn) {
+    for (let i = 0; i < amount; i++) {
+      const index = Math.floor(Math.random() * total);
+      const row = Math.floor(index / numCols);
+      const col = index % numCols;
+      const cell = grid[col][row];
+      fn(cell, col, row);
+    }
   }
-
-  setInterval(function changeRunes() {
-    container.querySelectorAll('.rune.fresh').forEach(element => element.classList.remove('fresh'));
-    const total = numCols * numRows;
-    for (let i = 0; i < total / 3; i++) {
-      const randomIndex = Math.floor(Math.random() * total);
-      const row = Math.floor(randomIndex / numCols);
-      const col = randomIndex % numCols;
-      const cell = container.children.item(col).children.item(row);
-      cell.innerText = randomRune();
-      cell.classList.add('fresh');
-    }
-  }, 1_000);
-}
+}, 1000);
 </script>
 
 <template>
   <CardBase>
     <img src="https://images.pokemontcg.io/sm10/33_hires.png">
-    <div class="card-runes" ref="$el"></div>
+    <div class="card-runes" ref="$el">
+      <span v-for="(col, x) in grid" class="rune-col">
+        <span v-for="cell in col" class="rune" :class="{fresh:cell.fresh}">
+          {{ cell.text }}
+        </span>
+      </span>
+    </div>
+    <div class="card-glare"></div>
   </CardBase>
 </template>
 <style scoped>
@@ -59,33 +67,44 @@ function setupRunes(container) {
   font-size: 20px;
   display: flex;
   justify-content: space-between;
-  mix-blend-mode: color-dodge;
+  mix-blend-mode: plus-lighter;
   opacity: var(--glare-opacity);
 
   mask-image: radial-gradient(
       farthest-corner circle at var(--pointer-x) var(--pointer-y),
-      hsla(0, 0%, 100%, 1) 10%,
-      hsla(0, 0%, 100%, 0.8) 40%,
-      hsla(0, 0%, 0%, 0) 90%
+      #ffffffff 10%,
+      #ffffffcc 40%,
+      #ffffff00 90%
   );
 
-  & >>> .rune-col {
+  & .rune-col {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
   }
 
-  & >>> .rune {
+  & .rune {
     color: transparent;
     text-shadow: rgba(0, 0, 0, 0.1) 0 0 3px;
     transition: all 1s ease-in-out;
     flex-grow: 1;
     flex-basis: 0;
-  }
 
-  & >>> .rune.fresh {
-    text-shadow: green 0 0 5px, lightgreen 0 0 10px, lightgreen 0 0 10px, lightgreen 0 0 10px;
-    color: white;
+    &.fresh {
+      text-shadow: lightgreen 0 0 5px, lightgreen 0 0 10px;
+      color: white;
+    }
   }
+}
+
+.card-glare {
+  background: radial-gradient(
+      farthest-corner circle at var(--pointer-x) var(--pointer-y),
+      hsla(0, 0%, 100%, 0.8) 10%,
+      hsla(0, 0%, 100%, 0.65) 20%,
+      hsla(0, 0%, 0%, 0.5) 90%
+  );
+  mix-blend-mode: overlay;
+  opacity: var(--glare-opacity);
 }
 </style>
