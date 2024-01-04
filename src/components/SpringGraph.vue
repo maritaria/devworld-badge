0462060d867da26ea60b2238d45c99108e33af0e
bramkamies <bramkamies@passionatepeople.io>
2024-01-04 10:55:36 +0100

    Remove unused canvas from example

--- src/components/SpringGraph.vue
+++ src/components/SpringGraph.vue
@@ -7,23 +7,11 @@ const $canvas = ref(null);
 const $value = ref(20);
 const $spring = useSpring(() => $value.value);
 
-onMounted(() => {
-  if (!$canvas.value) throw new Error('Missing $canvas');
-
-  renderGraph($canvas.value);
-
-});
-
-/** @param {HTMLCanvasElement} canvas */
-function renderGraph(canvas) {
-
-}
 </script>
 
 <template>
-  <canvas ref="$canvas" width="300" height="300" />
-  <input type="range" min="0" max="100" v-model="$value">
   <span>Value: {{ $value }}</span>
+  <input type="range" min="0" max="100" v-model="$value">
   <span>Spring: {{ $spring }}</span>
   <input type="range" min="0" max="100" :value="$spring">
 </template>
