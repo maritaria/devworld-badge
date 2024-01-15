2f56e5365345c6f2f2b0ca1234e7ce7ec7012b70
bramkamies <bramkamies@passionatepeople.io>
2024-01-15 16:46:37 +0100

    Disable routing failure text for the home route

--- src/components/RouteNotFound.vue
+++ src/components/RouteNotFound.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1>Routing failure</h1>
-  <p>
-    Route <code>{{$route.path}}</code> not found!
-  </p>
+  <template v-if="$route.path !== '/'">
+    <h1>Routing failure</h1>
+    <p>
+      Route <code>{{ $route.path }}</code> not found!
+    </p>
+  </template>
 </template>
