f8e9ec5990a6838d2e6663cfe9db74a4ba777982
bramkamies <bramkamies@passionatepeople.io>
2024-01-15 17:08:51 +0100

    Remove unused imports from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,6 @@
 <script setup>
 import {computed, provide, ref, watchEffect} from "vue";
 import {useInterval, useLocalStorage} from "@vueuse/core";
-import CardBase from "./components/CardBase.vue";
-import V1Shine from "./components/V1Shine.vue";
-import V1Sparkle from "./components/V1Sparkle.vue";
-import GlareV2 from "./components/V2Glare.vue";
-import GlareV3 from "./components/V3Glare.vue";
-import ShineRainbow from "./components/V3RainbowShine.vue";
-import MatrixRunes from "./components/MatrixRunes.vue";
-import Foil from "./components/Foil.vue";
-import GlareReverseHolo from "./components/GlareReverseHolo.vue";
-import DevWorldTicket from "./components/DevWorldTicket.vue";
-import DevWorldNeon from "./components/DevWorldNeon.vue";
-import DevWorldBadges from "./components/DevWorldBadges.vue";
-import DevWorldStandaloneBadge from "./components/DevWorldStandaloneBadge.vue";
-import ReglBadge from "./components/ReglBadge.vue";
-import ReglFoil from "./components/ReglFoil.vue";
-import LinearGradientLineExperiment from "./components/LinearGradientLineExperiment.vue";
-import ReglBoxShadow from "./components/ReglBoxShadow.vue";
-import ReglLayeringTest from "./components/ReglLayeringTest.vue";
-import ReglTiltedPanel from "./components/ReglTiltedPanel.vue";
-import ReglCube from "./components/ReglCube.vue";
-import ReglRecorder from "./components/ReglRecorder.vue";
-import SpringGraph from "./components/SpringGraph.vue";
-import ReglTiltedCard from "./components/ReglTiltedCard.vue";
-import ReglNeonText from "./components/ReglNeonText.vue";
-import ReglMatrixRain from "./components/ReglMatrixRain.vue";
-import ReglPlusLighter from "./components/ReglPlusLighter.vue";
-import ReglAvatar from "./components/ReglAvatar.vue";
-import SilhouettePicker from "./components/AvatarPicker.vue";
-import ReglPersonalCard from "./components/ReglPersonalCard.vue";
-import CanvasTextRenderer from "./components/CanvasTextRenderer.vue";
 import NavigationMenu from "./components/NavigationMenu.vue";
 
 const autoPointer = useLocalStorage('cards.autoPointer', 'disabled');
