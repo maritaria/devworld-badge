ba4572d62ffd6174669a0e9b9cffc41847a67fd2
bramkamies <bramkamies@passionatepeople.io>
2024-01-22 17:43:25 +0100

    Set vite build to library-mode

---
+++ src/library.js
@@ -0,0 +1,7 @@
+// Components
+export {default as PersonalCard} from "./components/PersonalCard.vue";
+
+// Functions
+export {pxRatio} from "./canvas.js";
+export {loadImageFromBlob} from "./regl/utilities.js";
+export {createImage} from "./resources.js";
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
+import {resolve} from 'node:path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  // Build in library-mode, https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/library.js'),
+      name: 'DevWorldBadgeLib',
+      fileName: 'devworld-badge',
+    },
+    minify: false,
+    rollupOptions: {
+      external: ['vue', 'regl'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+          regl: 'createREGL',
+        },
+      },
+    },
+    sourcemap: true,
+  },
 })
